74982d051e7019fca5e9000297ce7ebefb521477
EvStarysh <ev.starysh@gmail.com>
2023-08-18 16:35:19 +0300

    update

--- src/test/java/ru/marketYandex/StaryshTest.java
+++ src/test/java/ru/marketYandex/StaryshTest.java
@@ -34,8 +34,8 @@ public class StaryshTest {
         WebElement fieldMin = driver.findElement(By.xpath("//span[label[text()='Диагональ экрана (точно), \" от']]//input[@type='text']"));
         fieldMin.sendKeys("3", Keys.ENTER);
 
-        WebElement checkbox1 = (new WebDriverWait(driver, Duration.ofSeconds(2))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[contains(@data-zone-data, 'Производитель')]//label)[1]"))));
+        WebElement checkbox1 = (new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[contains(@data-zone-data, 'Производитель')]//span)[1]"))));
         checkbox1.click();
 
         WebElement checkbox2 = driver.findElement(By.xpath("(//div[contains(@data-zone-data, 'Производитель')]//label)[2]"));
